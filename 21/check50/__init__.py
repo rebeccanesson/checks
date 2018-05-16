ab74b1242ce350286899d5abe4be734899c17285
Rebecca Nesson <rebeccanesson@gmail.com>
2018-05-16 13:33:53 -0400

    adding 21

---
+++ 21/check50/__init__.py
@@ -0,0 +1,29 @@
+from check50 import *
+
+class TwentyOne(Checks): 
+
+	@check()
+	def exists(self):
+		"""21.c exists"""
+		self.require("21.c")
+
+	@check("exists")
+	def compiles(self):
+		"""21.c compiles"""
+		self.spawn("clang -o 21 21.c -lcs50 -lm").exit(0)
+
+	@check("compiles")
+	def test_123456(self):
+		"""123456 produces 21"""
+		self.spawn("./21").stdin("1").stdin("2").stdin("3").stdin("4").stdin("5").stdout("21\n", "21\n").exit(0)
+
+	@check("compiles")
+	def test_55555(self): 
+		"""55555 produces 25"""
+		self.spawn("./21").stdin("5").stdin("5").stdin("5").stdin("5").stdin("5").stdout("25\n", "25\n").exit(0)
+
+	@check("compiles")
+	def test_33(self): 
+		"""33 produces 33"""
+		self.spawn("./21").stdin("33").stdout("33\n").exit(0)
+
