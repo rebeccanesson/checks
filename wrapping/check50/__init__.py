d88ec312ccb9f0ae8ad552d903397d7c0df66538
Rebecca Nesson <rebeccanesson@gmail.com>
2018-05-16 11:02:41 -0400

    escaping in regexps

--- wrapping/check50/__init__.py
+++ wrapping/check50/__init__.py
@@ -15,10 +15,10 @@ class Wrapping(Checks):
 	@check("compiles")
 	def test_7_5_2(self):
 		"""7, 5, 2 produces 15 x 11"""
-		self.spawn("./wrapping").stdin("7").stdin("5").stdin("2").stdout("Length: 15.000000 Width: 11.000000\n", "Length: 15.000000 Height: 11.000000\n").exit(0)
+		self.spawn("./wrapping").stdin("7").stdin("5").stdin("2").stdout("Length: 15\.000000 Width: 11\.000000\n", "Length: 15.000000 Height: 11.000000\n").exit(0)
 
 	@check("compiles")
 	def test_float_sides(self): 
 		"""6.5, 6.5, 1 produces 16 x 8.5"""
-		self.spawn("./wrapping").stdin("6.5").stdin("6.5").stdin("1").stdout("Length: 16.000000 Width: 11.000000\n", "Length: 16.000000 Width: 8.500000\n").exit(0)
+		self.spawn("./wrapping").stdin("6.5").stdin("6.5").stdin("1").stdout("Length: 16\.000000 Width: 8\.500000\n", "Length: 16.000000 Width: 8.500000\n").exit(0)
 
