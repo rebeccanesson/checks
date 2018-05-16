c55bab04bae43301b7c115244527bcaf73f791ce
Rebecca Nesson <rebeccanesson@gmail.com>
2018-05-16 13:37:30 -0400

    fixing 21 checks

--- 21/check50/__init__.py
+++ 21/check50/__init__.py
@@ -15,15 +15,15 @@ class TwentyOne(Checks):
 	@check("compiles")
 	def test_123456(self):
 		"""123456 produces 21"""
-		self.spawn("./21").stdin("1").stdin("2").stdin("3").stdin("4").stdin("5").stdout("21\n", "21\n").exit(0)
+		self.spawn("./21").stdin("1").stdin("2").stdin("3").stdin("4").stdin("5").stdout("Sum: 21\n", "Sum: 21\n").exit(0)
 
 	@check("compiles")
 	def test_55555(self): 
 		"""55555 produces 25"""
-		self.spawn("./21").stdin("5").stdin("5").stdin("5").stdin("5").stdin("5").stdout("25\n", "25\n").exit(0)
+		self.spawn("./21").stdin("5").stdin("5").stdin("5").stdin("5").stdin("5").stdout("Sum: 25\n", "Sum: 25\n").exit(0)
 
 	@check("compiles")
 	def test_33(self): 
 		"""33 produces 33"""
-		self.spawn("./21").stdin("33").stdout("33\n").exit(0)
+		self.spawn("./21").stdin("33").stdout("Sum: 33\n").exit(0)
 
