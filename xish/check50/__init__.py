ac72584c7246ccd1def26a8a603b2a881a93dbf7
Rebecca Nesson <rebeccanesson@gmail.com>
2018-05-21 18:58:19 -0400

    checks for xish

--- xish/check50/__init__.py
+++ xish/check50/__init__.py
@@ -12,4 +12,13 @@ class Xish(Checks):
 		"""xish.c compiles"""
 		self.spawn("clang -o xish xish.c -lcs50 -lm").exit(0)
 
+	@check("compiles")
+	def test_elf(self):
+		"""elf is elfish"""
+		self.spawn("./xish").stdin("elf").stdin("elf").stdout("YES!\n", "YES!\n").exit(0)
+	
+	@check("compiles")
+	def test_troll(self):
+		"""troll is not elfish"""
+		self.spawn("./xish").stdin("elf").stdin("troll").stdout("NO\n", "NO\n").exit(0)
 
