55b033d265ce9d3ca65284c9d1a5f9ce75cb17dd
Rebecca Nesson <rebeccanesson@gmail.com>
2018-05-21 17:26:01 -0400

    fixing arraymagic 2 checks

--- arraymagic2/check50/__init__.py
+++ arraymagic2/check50/__init__.py
@@ -36,4 +36,4 @@ class ArrayMagic2(Checks):
 	@check("compiles")
 	def test_not_magic_illegal_inputs_5(self): 
 		"""rejects size 2 and 6, 17 24 8 1 15 23 5 7 14 16 4 6 13 20 22 10 12 19 21 3 11 18 25 2 9"""
-		self.spawn("./arraymagic2").stdin("2").stdin("6").stdin("5").stdin("17").stdin("24").stdin("8").stdin("1").stdin("15").stdin("23").stdin("5").stdin("7").stdin("14").stdin("16").stdin("4").stdin("6").stdin("13").stdin("20").stdin("22").stdin("10").stdin("12").stdin("19").stdin("21").stdin("3").stdin("11").stdin("18").stdin("25").stdin("2").stdin("9").stdout("MAGIC!\n", "MAGIC!\n").exit(0)
+		self.spawn("./arraymagic2").stdin("2").stdin("6").stdin("5").stdin("17").stdin("24").stdin("8").stdin("1").stdin("15").stdin("23").stdin("5").stdin("7").stdin("14").stdin("16").stdin("4").stdin("6").stdin("13").stdin("20").stdin("22").stdin("10").stdin("12").stdin("19").stdin("21").stdin("3").stdin("11").stdin("18").stdin("25").stdin("2").stdin("9").stdout("NOT MAGIC\n", "NOT MAGIC\n").exit(0)
