682d170243b75d9556f5e8d2f94bd037b6f3bedc
Rebecca Nesson <rebeccanesson@gmail.com>
2018-05-21 14:22:40 -0400

    reverse checks fixes

--- reverse/check50/__init__.py
+++ reverse/check50/__init__.py
@@ -15,10 +15,10 @@ class Reverse(Checks):
 	@check("compiles")
 	def test_4(self):
 		"""1 2 3 4 produces 4 3 2 1"""
-		self.spawn("./reverse").stdin("4").stdin("1").stdin("2").stdin("3").stdin("4").stdout("4 3 2 1\n", "4 3 2 1\n").exit(0)
+		self.spawn("./reverse").stdin("4").stdin("1").stdin("2").stdin("3").stdin("4").stdout("4 3 2 1 \n", "4 3 2 1 \n").exit(0)
 
 	@check("compiles")
 	def test_illegal_input(self): 
 		"""negative and 0 inputs"""
-		self.spawn("./reverse").stdin("-4").stdin("0").stdin("1").stdin("5").stdout("5\n", "5\n").exit(0)
+		self.spawn("./reverse").stdin("-4").stdin("0").stdin("1").stdin("5").stdout("5 \n", "5 \n").exit(0)
 
