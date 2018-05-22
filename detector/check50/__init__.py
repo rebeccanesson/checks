59770c3018bd5a04ecf3c2c9b21a5bf0c85ba3e6
Rebecca Nesson <rebeccanesson@gmail.com>
2018-05-22 09:11:06 -0400

    adding detector checks

---
+++ detector/check50/__init__.py
@@ -0,0 +1,34 @@
+from check50 import *
+
+class Detector(Checks): 
+
+	@check()
+	def exists(self):
+		"""detector.c exists"""
+		self.require("detector.c")
+
+	@check("exists")
+	def compiles(self):
+		"""detector.c compiles"""
+		self.spawn("clang -o detector detector.c -lcs50 -lm").exit(0)
+
+	@check("compiles")
+	def test_foobar(self):
+		"""foobar is a great word"""
+		self.spawn("./detector").stdin("foobar").stdin("Foobar is a great word").stdout("YES!\n", "YES!\n").exit(0)
+	
+	@check("compiles")
+	def test_terrible(self):
+		"""foobar is a terrible word"""
+		self.spawn("./detector").stdin("terrible").stdin("Foobar is a terrible word").stdout("YES!\n", "YES!\n").exit(0)
+
+	@check("compiles")
+	def test_great(self):
+		"""foobar is a great and terrible word"""
+		self.spawn("./detector").stdin("great").stdin("Foobar is a terrible word").stdout("NO\n", "NO\n").exit(0)
+	
+	@check("compiles")
+	def test_foo(self):
+		"""that is a lot of foo"""
+		self.spawn("./detector").stdin("FOOBAR").stdin("Wow, that is a lot of foo!").stdout("YES!\n", "YES!\n").exit(0)
+
